00c34290fadc85135fc655393276e8f3b47e80d5
gemmas95 <gemma1030@gmail.com>
2020-10-08 12:41:57 +0200

    added exericice 2 and 3

---
+++ exercice2/exercice2.js
@@ -0,0 +1,25 @@
+/* eslint-disable no-debugger */
+/* eslint-disable no-console */
+/* eslint-disable no-plusplus */
+
+function maxSubarraySum(array, number) {
+  let sum = 0;
+  let arraySum = 0;
+  for (let j = 0; j <= array.length; j++) {
+    sum = 0;
+    debugger;
+    for (let i = 0; i - 1 < number; i++) {
+      console.log("1n sum...", sum);
+
+      sum += array[i];
+      console.log("2n sum...", sum);
+      arraySum = [sum, ...arraySum];
+      console.log("1n arraySum-...", arraySum);
+    }
+  }
+  console.log("2n arraySum...", arraySum);
+
+  return arraySum;
+}
+
+maxSubarraySum([1, 2, 3], 2);
---
+++ exercice3/exercice3.js
@@ -0,0 +1,28 @@
+/* eslint-disable no-console */
+/* eslint-disable no-debugger */
+/* eslint-disable no-plusplus */
+
+// NOT WORKING WELL, second example not passing
+
+function averagePair(array, number) {
+  const averages = [];
+  for (let i = 0; i + 1 < array.length; i++) {
+    averages[i] = (array[i] + array[i + 1]) / 2;
+  }
+  console.log(averages);
+  return console.log(averages.some((element) => element === number));
+}
+averagePair([1, 3, 3, 5, 6, 7, 10, 12, 19], 8);
+
+/* function averagePair(array, number) {
+  let averages;
+  const arrayAverages = array.reduce((accum, currVal) => {
+    averages = [currVal + accum / 2, averages];
+    console.log(averages);
+    return averages;
+  });
+  console.log(arrayAverages);
+  return arrayAverages.some((element) => number === element);
+}
+averagePair([1, 2, 3], 2.5);
+ */
