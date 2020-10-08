c098f47a4ef601073471575a06d1c0249a864acc
gemmas95 <gemma1030@gmail.com>
2020-10-08 18:34:52 +0200

    installes npm in order to do the tests

--- exercise1/exercise1.test.js
+++ exercise1/exercise1.test.js
@@ -1,3 +1,5 @@
+/* eslint-disable no-unused-expressions */
+/* eslint-disable no-undef */
 const sinon = require("sinon");
 const { expect } = require("chai");
 const { describe } = require("mocha");
@@ -5,6 +7,9 @@ const { describe } = require("mocha");
 const exercise1 = require("./exercise1");
 
 describe("Exercise1", () => {
+  afterEach(() => {
+    sinon.restore();
+  });
   it("should return an array value", () => {
     expect(true).to.be.true;
   });
